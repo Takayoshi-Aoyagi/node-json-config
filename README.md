2837507b107df1ea78f5e1f0bec13f8c1d3f7ae6
Takayoshi Aoyagi <takayoshi_aoyagi@waku-2.com>
2015-03-13 18:12:57 +0900

    Initial Revision

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # node-json-config
 Read and Write configurations to JSON file.
+
+# How to use
+
+## initialize
+
+Create Config instance with JSON path.
+Configurations are automatically loaded from the JSON file.
+
+```
+var conf = new Config("./config.json");
+```
+
+## Read configuration
+
+You can access configurations using dot notation.
+
+Here is sample JSON.
+
+```
+{
+  "one": 1,
+  "a": {
+    "b": {
+	  "c": "ABC"
+	}
+  }
+}
+```
+
+```
+conf.get("one")    // 1
+conf.get("a.b.c")  // "ABC"
+```
+
+## Write configuration
+
+Also you can put new configuration using dot notation.
+
+```
+{
+  "one": 1,
+  "a": {
+    "b": {
+	  "c": "ABC"
+	}
+  }
+}
+```
+
+```
+conf.put("two", 2);
+conf.put("x.y.z", "XYZ");
+```
+
+```
+{
+  "one": 1,
+  "a": {
+    "b": {
+	  "c": "ABC"
+	}
+  },
+  "two": 2,
+  "x": {
+    "y": {
+	  "z": "XYZ"
+	}
+  }
+}
+```
+
+## Save configurations
+
+```
+conf.save();
+```
---
+++ test/test.js
@@ -0,0 +1,61 @@
+var assert = require("assert");
+var Config = require('../lib/config');
+
+var debug = false;
+
+function log(str) {
+    if (debug) {
+	console.log(str);
+    }
+}
+
+describe('Config', function () {
+
+    var path = "./config.json";
+    var conf = new Config(path);
+    
+    describe('#get()', function () {
+	it('foo => bar', function () {
+	    var key = "foo";
+	    var val = conf.get(key);
+	    assert.equal("bar", val);
+	});
+
+    	it('a.b.c.d => 999', function () {
+	    var key = "a.b.c.d";
+	    var val = conf.get(key);
+	    assert.equal(999, val);
+	});
+
+    	it('x.y.z => undefined', function () {
+	    var key = "x.y.z";
+	    var val = conf.get(key);
+	    assert.equal(undefined, val);
+	});
+    });
+
+    describe('#put()', function () {
+	it ('hoge => fuga', function () {
+	    var key = "hoge",
+		val = "fuga";
+	    log(conf.toString());
+	    assert.equal(undefined, conf.get(key));
+	    conf.put(key, val);
+	    assert.equal(val, conf.get(key));
+	    log("--------");
+	    log(conf.toString());
+	});
+
+    	it('x.y.z => undefined, then x.y.z = "XYZ"', function () {
+	    var key = "x.y.z",
+		val = "XYZ";
+	   log(conf.toString());
+	    assert.equal(undefined, conf.get(key));
+	    conf.put(key, val);
+	    assert.equal(val, conf.get(key));
+	   log("--------");
+	   log(conf.toString());
+	});
+
+    });    
+});
